4d358e9bd1f09b5f30c2b692e497b6798d7a6e2f
Vadym <ghoast@ukr.net>
2014-03-01 02:53:06 +0200

    cosmetic

--- StringComparison/StringComparison.h
+++ StringComparison/StringComparison.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 #include "Levenshtein.h"
 
 #include <string>
@@ -8,30 +7,28 @@
 
 namespace StringComparison
 {
-	enum MODE { CASE_SENSITIVE, CASE_INSENSITIVE };
+    enum MODE { CASE_SENSITIVE, CASE_INSENSITIVE };
 
-	template <typename _String_T>
-	float getSimilarity(_String_T &s1, _String_T &s2, MODE mode = CASE_INSENSITIVE);
+    template <typename _String_T>
+    float getSimilarity(_String_T &s1, _String_T &s2, MODE mode = CASE_INSENSITIVE);
 
 
-	template <typename _String_T>
-	float getSimilarity(_String_T &s1, _String_T &s2, MODE mode)
-	{
-		_String_T lstr = s1;
-		_String_T rstr = s2;
+    template <typename _String_T>
+    float getSimilarity(_String_T &s1, _String_T &s2, MODE mode)
+    {
+        _String_T lstr = s1;
+        _String_T rstr = s2;
 
-		if (mode == StringComparison::CASE_INSENSITIVE) {
-			std::transform(lstr.begin(), lstr.end(), lstr.begin(), ::tolower);
-			std::transform(rstr.begin(), rstr.end(), rstr.begin(), ::tolower);
-		}
+        if (mode == StringComparison::CASE_INSENSITIVE) {
+            std::transform(lstr.begin(), lstr.end(), lstr.begin(), ::tolower);
+            std::transform(rstr.begin(), rstr.end(), rstr.begin(), ::tolower);
+        }
 
-		float levenshtein = Levenshtein(lstr.c_str(), rstr.c_str());
+        float levenshtein = Levenshtein(lstr.c_str(), rstr.c_str());
 
-		int maxLen = std::max(lstr.length(), rstr.length());
+        int maxLen = std::max(lstr.length(), rstr.length());
 		
-		return  (maxLen - levenshtein) / maxLen;
-	}
+        return  (maxLen - levenshtein) / maxLen;
+    }
 
 }
-
-
